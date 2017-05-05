f47882b1541bebd646e95bfa810d97ccb47c556b
kwokth2016 <kwokth2016@gmail.com>
2017-05-05 17:44:44 +0800

    update the arduino

--- smart/smart.ino
+++ smart/smart.ino
@@ -7,10 +7,24 @@ dht DHT;
 #define water A0
 #define water2 A2
 #define soil A3
+
+
+#define pump1 4
+#define pump2 5
+#define ledstrip 8
 #define buzzer 9
+#define atomizer 10
+#define fan1 11
+#define fan2 12
+
+
 void setup(){
   Serial.begin(9600);
   pinMode(buzzer, OUTPUT);
+  pinMode(ledstrip,OUTPUT);
+  pinMode(atomizer,OUTPUT);
+  pinMode(fan1,OUTPUT);
+  pinMode(fan2,OUTPUT);
 }
 
 void prepareJson(){
@@ -47,10 +61,10 @@ void alert(){
   int waterlevel2 = analogRead(water2);
   if(waterlevel2<50)
   {
-   tone(buzzer, 500); // Send 1KHz sound signal...
+   tone(buzzer, 500); // Send 0.5KHz sound signal...
    delay(1000);        // ...for 1 sec
    noTone(buzzer);     // Stop sound...
-   delay(1000);        // ...for 1sec
+//   delay(1000);        // ...for 1sec
   }
   int temp = DHT.temperature;
   if(temp<15)
@@ -58,21 +72,41 @@ void alert(){
    tone(buzzer, 1000); // Send 1KHz sound signal...
    delay(1000);        // ...for 1 sec
    noTone(buzzer);     // Stop sound...
-   delay(1000);        // ...for 1sec
+//   delay(1000);        // ...for 1sec
   }
   if(temp>35)
   {
-   tone(buzzer, 2000); // Send 1KHz sound signal...
+   tone(buzzer, 2000); // Send 2KHz sound signal...
    delay(1000);        // ...for 1 sec
    noTone(buzzer);     // Stop sound...
-   delay(1000);        // ...for 1sec
+//   delay(1000);        // ...for 1sec
   }
 }
 
+void response(){
+  int lightlevel = analogRead(light);
+  int temperaturelevel = DHT.temperature;
+  //led strip
+  if(lightlevel>400)
+    analogWrite(ledstrip,255);
+  else
+    analogWrite(ledstrip,0);
+  //fan and exhaust fan  
+  if(temperaturelevel > 35){
+    digitalWrite(fan1,HIGH);
+    analogWrite(fan2,255);
+  }
+  else{
+    digitalWrite(fan1,HIGH);
+    analogWrite(fan2,0);
+  }
+  
+}
 void loop()
 {
   int chk = DHT.read11(DHT11_PIN);
   prepareJson();
   alert();
+  response();
   delay(1000);
 }
